876758bdb05d73a69a4641fa9241e1d1f8acc727
Januka-Peiris <januka.peiris@assuredinsights.com>
2025-11-21 21:56:26 +0000

    Tweaks

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,8 +3,8 @@ import { styles } from "../styles";
 
 const Hero = () => {
   return (
-    <section className="relative w-full h-screen mx-auto bg-gradient-to-b from-[#0a192f] via-[#0f1f35] to-[#0a192f]">
-      <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
+    <section className="relative w-full h-screen min-h-[700px] mx-auto bg-gradient-to-b from-[#0a192f] via-[#0f1f35] to-[#0a192f]">
+      <div className={`${styles.paddingX} absolute inset-0 top-[100px] md:top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
         <div className="flex flex-col justify-center items-center mt-5">
           <motion.div
             initial={{ scale: 0 }}
